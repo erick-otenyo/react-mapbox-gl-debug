5a0ebce930fca73a66a570f63f85f14d0af392f2
Erick Otenyo <otenyo.erick@gmail.com>
2018-01-17 12:18:42 +0300

    add bounds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import ReactMap from 'react-mapbox-gl';
+import React, { Component } from "react";
+import ReactMap from "react-mapbox-gl";
 
-const accessToken = "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2o0MHp2cGtiMGFrajMycG5nbzBuY2pjaiJ9.QDApU0XH2v35viSwQuln5w";
+const accessToken =
+  "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2o0MHp2cGtiMGFrajMycG5nbzBuY2pjaiJ9.QDApU0XH2v35viSwQuln5w";
 const style = "mapbox://styles/mapbox/streets-v9";
 
 const Map = ReactMap({
@@ -9,9 +10,11 @@ const Map = ReactMap({
 });
 
 const mapStyle = {
-  height: '100vh',
-  width: '100vw'
+  height: "100vh",
+  width: "100vw"
 };
+
+const bounds = [[33.90958, -4.674901], [41.89101, 5.482579]];
 
 class App extends Component {
   render() {
@@ -19,6 +22,8 @@ class App extends Component {
       <Map
         style={style}
         containerStyle={mapStyle}
+        fitBounds={bounds}
+        fitBoundsOptions={{ padding: 40 }}
       />
     );
   }
